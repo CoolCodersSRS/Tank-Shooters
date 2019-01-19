d9dcd312697c8d808984867f95e86119a95ec3fb
CoolCodersSRS <woofcat123@gmail.com>
2019-01-19 18:18:06 +0530

    first commit

---
+++ Tank-Class_Defintions.py
@@ -0,0 +1,138 @@
+import pygame
+import time
+import math
+from TankSettings import *
+vd = pygame.math.Vector2
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self,game,kind,pos,ang):
+        pygame.sprite.Sprite.__init__(self)
+        self.game = game
+        self.ang = ang
+        if kind == 1:
+            self.image = pygame.Surface((5,5))
+            self.image.fill(RED)
+        self.pos = vd(pos.x,pos.y)
+        self.rect = self.image.get_rect()
+        self.rect.center = (pos.x,pos.y)
+        self.vel = vd(BULLET_SPEED * math.cos(math.radians(self.ang)), BULLET_SPEED * math.sin(math.radians(self.ang)))
+
+    def update(self):
+        self.pos += self.vel
+        self.rect.center = self.pos
+        collide = pygame.sprite.spritecollide(self,self.game.platforms,False)
+        if self.pos.x > WIDTH:
+            self.kill()
+        if self.pos.x < 0:
+            self.kill()
+        if collide:
+            self.kill()
+
+class Nozzle(pygame.sprite.Sprite):
+    def __init__(self,player,pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.main_image_nozzle = pygame.image.load('???.gif')
+        self.image = self.main_image_nozzle
+        self.player = player
+        self.rect = self.image.get_rect()
+        self.rect.centerx = pos.x + 15
+        self.rect.centery = pos.y - 15
+        self.angle = 0
+        self.pos = pos
+
+    def update(self):
+        keys = pygame.key.get_pressedd()
+        if keys[pygame.K_d]:
+            self.angle += 2
+        if keys[pygame.K_f]:
+            self.angle += -2
+        self.rect.centerx = self.pos.x + 15
+        self.rect.centery = self.pos.y - 15
+        self.image = pygame.transform.rotate(self.main_image_nozzle,self.angle)
+        self.xx = self.rect.centerx
+        self.yy = self.rect.centery
+        self.rect = self.image.get_rect()
+        self.rect.center = (self.xx,self.yy)
+
+
+    def Function1(self,pos):
+        self.rect.centerx = pos.x + 15
+        self.rect.centery = pos.y - 15
+        self.pos.x = pos.x
+        self.pos.y = pos.y
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self,game,sprl):
+        pygame.sprite.Sprite.__init__(self)
+        self.game = game
+        self.sprl = sprl
+        self.main_image_player  = pygame.image.load('player.gif')
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH * 3/4, HEIGHT / 2)
+        self.pos = vd(WIDTH * 3/4, HEIGHT / 2)
+        self.vel = vd(0,0)
+        self.acc = vd(0,0)
+        self.dir = 2
+        self.nozzle = Nozzle(self,self.pos)
+        self.sprl.add(nozzle)
+    def jump(self):
+        self.rect.x += 1
+        collide = pygame.sprite.spritecollide(self,self.game.platforms,False)
+        self.rect.x += -1
+        if collide:
+            self.vel.y = JUMP
+
+    def update(self):
+        self.acc = vd(0,GRAVITY)
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.acc.x += -ACCELERATION
+            self.dir = 1
+            self.nozzle.angle = 180
+        if keys[pygame.K_RIGHT]:
+            self.acc.x += ACCELERATION
+            self.dir = 2
+            self.nozzle.angle = 0
+
+        self.acc.x += self.vel.x * FRICTION
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+        if self.pos.x > WIDTH:
+            self.pos.x = WIDTH
+        if self.pos.x = 0:
+            self.pos.x = 0
+        self.rect.midbottom = self.pos
+        self.nozzle.Function1(self.pos)
+            
+        
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                      
+        
+
+
+
+
+
+
+
+                      
---
+++ platformer.py
@@ -0,0 +1,86 @@
+import pygame
+import random
+import math
+from TankClassDefintions import *
+from TankSettings import *
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.mixer.init()
+        self.screen = pygame.display.set_mode((WIDTH,HEIGHT))
+        pygame.display.set_caption(TITLE)
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+
+    def update(self):
+        self.sprl.update()
+        if self.player.vel.y > 0:
+            collide = pygame.sprite.spritecollide(self.player,self.platforms, False)
+            if collide:
+                self.player.pos.y = collide[0].rect.top + 1
+                self.player.vel.y = 0
+        if self.kills >= KILLCOUNT:
+            self.playing = False
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                if self.playing:
+                    self.playing = False
+                self.running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    self.bullet = Bullet(self,1,self.player.nozzle.pos - (20,20),0-self.player.nozzle.angle)
+                    self.sprl.add(self.bullet)
+                if event.key == pygame.K_UP:
+                    self.player.jump()
+
+    def draw(self):
+        self.screen.fill(BLACK)
+        self.sprl.draw(self.screen)
+        pygame.display.flip()
+
+    def new_game(self):
+        self.kills = 0
+        self.platforms = pygame.sprite.Group()
+        self.sprl = pygame.sprite.Group()
+        self.player = Player(self,self.sprl)
+        ground = Platform(0,HEIGHT - 40,WIDTH,40)
+        self.sprl.add(ground)
+        self.platforms.add(ground)
+        self.sprl.add(self.player)
+        for p in PLATFORMYS:
+            plat = Platform(*p)
+            self.sprl.add(plat)
+            self.platforms.add(plat)
+        self.run()
+
+    def showstartscreen(self):
+        pass
+    def showgameoverscreen(self):
+        pass
+    def display_text(self,text,size,color,x,y):
+        font = pygame.font.Font('arial',size)
+        text_surface = font.render(text,True,color)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (x,y)
+        self.screen.blit(text_surface,text_rect)
+
+g = Game()
+g.showstartscreen()
+while g.running:
+    g.new_game()
+    if not self.playing:
+        g.showgameoverscreen()
+pygame.quit()
+                
+            
